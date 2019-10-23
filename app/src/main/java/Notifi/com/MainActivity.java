0361457ec3376af7698fb641b7fecb114fb9a41b
dokysp <uhug@naver.com>
2019-10-23 14:50:53 +0900

    기능 완성 및 코드 정리

--- app/src/main/java/Notifi/com/MainActivity.java
+++ app/src/main/java/Notifi/com/MainActivity.java
@@ -2,12 +2,12 @@ package Notifi.com;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationManagerCompat;
-
 import java.util.Set;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,36 +16,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        // <특별한 접근 -> 알림 접근 혀용> 확인
         if (!isPermissionGranted()) {
-            // 5.0 부터 인텐트 명시적 선언이 안되게 됨.
-            // https://bongjacy.tistory.com/entry/%EC%95%88%EB%93%9C%EB%A1%9C%EC%9D%B4%EB%93%9C-%EC%84%9C%EB%B9%84%EC%8A%A4-%EC%9D%B8%ED%85%90%ED%8A%B8-%EC%83%9D%EC%84%B1%ED%95%A0-%EB%95%8C-%EB%B0%9C%EC%83%9D-%EC%98%A4%EB%A5%98-Service-Intent-must-be-explicit
-
-            Log.e("=====", "Open Setting");
-            // 시스템 설정 여는 코드
-
+            // 접근 혀용이 되어있지 않다면 1. 메시지 발생 / 2, 설정으로 이동시킴
+            Toast.makeText(getApplicationContext(), getString(R.string.app_name)+" 앱의 알림 권한을 허용해주세요.", Toast.LENGTH_LONG).show();
             startActivity(new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"));
         }
 
-            // 서비스 시작 코드
-//            // https://bitsoul.tistory.com/147
-              Intent service = new Intent(this, NotificationListener.class);
-              startService(service);
-
+        // 서비스 시작
+        // NotificationListenerService의 경우, Rebind() 메소드 실행 전까지 서비스가 종료되지 않는 것으로 확인됨.
+        Intent service = new Intent(getApplicationContext(), NotificationListener.class);
+        startService(service);
     }
+
 
     private boolean isPermissionGranted() {
         // 노티수신을 확인하는 권한을 가진 앱 모든 리스트
         Set<String> sets = NotificationManagerCompat.getEnabledListenerPackages(this);
-        // 위 리스트에 이 앱이 해당되어 있는가?
-        if (sets != null && sets.contains(getPackageName())) {
-            Log.e("permission", "true");
-            return true;
-        } else {
-            Log.e("permission", "false");
-            return false;
-        }
+        //  Notify앱의 알림 접근 허용이 되어있는가?
+        return sets != null && sets.contains(getPackageName());
     }
-
-
 }
--- app/src/main/java/Notifi/com/NotificationListener.java
+++ app/src/main/java/Notifi/com/NotificationListener.java
@@ -7,83 +7,70 @@ import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
 
+
+
 public class NotificationListener extends NotificationListenerService {
     public final static String TAG = "=====";
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.e(TAG, "[snowdeer] onCreate()");
+        Log.e(TAG, "onCreate():: ");
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.e(TAG, "[snowdeer] onDestroy()");
+        Log.e(TAG, "onDestroy():: ");
     }
 
     @Override
     public void onListenerConnected() {
         super.onListenerConnected();
-
-        Log.e(TAG, "[snowdeer] onListenerConnected()");
+        Log.e(TAG, "onListenerConnected():: ");
     }
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
+
+        // 새로운 알림이 생성되었을 때
         super.onNotificationPosted(sbn);
-        Log.e(TAG, "[snowdeer] onNotificationPosted()");
+        Log.e(TAG, "onNotificationPosted()");
 
         Notification notification = sbn.getNotification();
         Bundle extras = sbn.getNotification().extras;
-        String title = extras.getString(Notification.EXTRA_TITLE);
-        CharSequence text = extras.getCharSequence(Notification.EXTRA_TEXT);
+
+        String contentTitle = extras.getString(Notification.EXTRA_TITLE);
+        CharSequence contentText = extras.getCharSequence(Notification.EXTRA_TEXT);
         CharSequence subText = extras.getCharSequence(Notification.EXTRA_SUB_TEXT);
+        CharSequence bigText = extras.getCharSequence(Notification.EXTRA_BIG_TEXT);
+        CharSequence bigContentTitle = extras.getCharSequence(Notification.EXTRA_TITLE_BIG);
+
         Icon smallIcon = notification.getSmallIcon();
         Icon largeIcon = notification.getLargeIcon();
 
-        Log.e(TAG, "Posted ~ " +
-                " id: " + sbn.getId() +
-                " text : " + text +
-                " subText: " + subText);
-
-        //파이썬 서버에 sentence를 보내고 명사를 받아옵니다.
-        // 도균님 승재님 작업중..
-
-
-        //각 명사들의 value 값들을 디비에서 찾아옵니다.
-        //noun_list = 명사 <- 서버에서 받아온 명사 리스트를 쟤한테 넣어주세요
-
-        //NounTable을 생성하고 객체를 가져옵니다.
-//        DBHelper DBHelperelperHelper = new NounDBHelper(this);
-//        SQLiteDatabase db = DBHelper.getReadableDatabase();
-//
-//        int sentence_value = 0;
-//        for(int i=0; i<noun_list.size; i++) {
-//            //해당 명사의 value값을 가져옵니다.
-//            String sql = "select value from NounTable where sentence = '" + noun_list[i] + "'" ;
-//            // 쿼리실행
-//            Cursor c = db.rawQuery(sql, null);
-//            int value = c.getInt(1);
-//
-//            //각 명사의 value 값을 누적합니다.
-//            sentence_value += value;
-//        }
-//
-//        //도착한 노티와 예상 value 값을  디비에 저장합니다.
-//        String sql = "insert into NotiTable (sentence, value) values ('" + sentence + "', " + sentence_value + ")";
-//        db.execSQL(sql);
-
+        Log.e(TAG, "NotiPosted::  " +
+                " / id : " + sbn.getId() +
+                " / bigContentTitle : " + bigContentTitle +
+                " / bigText : " + bigText +
+                " / contentText : " + contentText +
+                " / subText : " + subText +
+                " / contentTitle : " + contentTitle +
+                " / icon : " + smallIcon.getResId() +
+                " / packageName : " + sbn.getPackageName()
+        );
     }
+
 
     @Override
     public void onNotificationRemoved(StatusBarNotification sbn) {
-        super.onNotificationRemoved(sbn);
-        Log.e(TAG, "[snowdeer] onNotificationRemoved()");
 
-        Log.e(TAG, "onNotificationRemoved ~ " +
+        // 알림이 지워졌을 때
+        super.onNotificationRemoved(sbn);
+        Log.e(TAG, "onNotificationRemoved():: ");
+
+        Log.e(TAG, "NotiIRemoved:: " +
                 " packageName: " + sbn.getPackageName() +
                 " id: " + sbn.getId());
     }
-
 }
